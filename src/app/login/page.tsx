68c7ab958d6668fb96e52e10dc2fdb7636b36770
garridomartin <martin.garrido@gmail.com>
2023-08-17 10:37:46 -0300

    prueba

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,6 @@
     Author: 🟣 Enoc Lima
 =====================================================*/
 
-
 'use client';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
@@ -14,74 +13,78 @@ import PrimaryButton from '@/components/buttons/primaryButton';
 import { BiLogInCircle } from 'react-icons/bi';
 import { FcGoogle } from 'react-icons/fc';
 import formatValidate from '@/assets/js/format_validates';
-
+import { useGetUserQuery } from '@/context/redux/api_handler/userApi';
 
 function Login() {
-    const [formData, setFormData] = useState({email: "", password: ""})
-    const [buttonDisabled, setButtonDisabled] = useState(true);
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-    }
-    
-    useEffect(() => {
-        formData.email && formData.password ? setButtonDisabled(false) : setButtonDisabled(true);
-    },[formData])
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = useGetUserQuery('/signIn');
+  };
 
-    return (
-        <div className={style.container}>
-            <div className={style.login}>
-                <div className={style.login__header}>
-                    <h1 className='title'>Iniciar sesión</h1>
-                </div>
-                <div className={style.login__body}>
-                    <form className={style.login__form}
-                        onSubmit={handleSubmit}
-                        >
-                        <ClassicInput type="email"
-                            name="email" 
-                            label="Ingrese su correo electrónico"
-                            errorMessage='Correo electrónico inválido'
-                            validator={formatValidate('any', 'email')}
-                            setFormData={setFormData} />
-                        <ClassicInput type="password" 
-                            name="password" 
-                            label="Contraseña"
-                            validator={formatValidate('any', 'minmax8_36')}
-                            setFormData={setFormData}
-                            errorMessage='Contraseña inválida'
-                            />
+  useEffect(() => {
+    formData.email && formData.password
+      ? setButtonDisabled(false)
+      : setButtonDisabled(true);
+  }, [formData]);
 
-                        <span className={style.forgot__container}>
-                        <Link className='link' href="/forgot-password">
-                            ¿Olvidaste tu contraseña?
-                        </Link>
-                        </span>
-
-                        <PrimaryButton text="Iniciar sesión" Icon={BiLogInCircle} disabled={buttonDisabled} />
-                    </form>
-
-                    <div className={style.login__footer}>
-                        <p className={style.text}>
-                            ¿No tienes una cuenta?  <Link className='link'
-                                href="/register">
-                                Registrate
-                            </Link>
-                        </p>
-                        <div>
-                            <Link className={style.googleLogin}  href="/google-login">
-                                <span className={style.text}>
-                                    Iniciar sesión con Google
-                                </span>
-                                <FcGoogle fontSize={30} />
-                            </Link>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-
+  return (
+    <div className={style.container}>
+      <div className={style.login}>
+        <div className={style.login__header}>
+          <h1 className='title'>Iniciar sesión</h1>
         </div>
-    )
+        <div className={style.login__body}>
+          <form className={style.login__form} onSubmit={handleSubmit}>
+            <ClassicInput
+              type='email'
+              name='email'
+              label='Ingrese su correo electrónico'
+              errorMessage='Correo electrónico inválido'
+              validator={formatValidate('any', 'email')}
+              setFormData={setFormData}
+            />
+            <ClassicInput
+              type='password'
+              name='password'
+              label='Contraseña'
+              validator={formatValidate('any', 'minmax8_36')}
+              setFormData={setFormData}
+              errorMessage='Contraseña inválida'
+            />
+
+            <span className={style.forgot__container}>
+              <Link className='link' href='/forgot-password'>
+                ¿Olvidaste tu contraseña?
+              </Link>
+            </span>
+
+            <PrimaryButton
+              text='Iniciar sesión'
+              Icon={BiLogInCircle}
+              disabled={buttonDisabled}
+            />
+          </form>
+
+          <div className={style.login__footer}>
+            <p className={style.text}>
+              ¿No tienes una cuenta?{' '}
+              <Link className='link' href='/register'>
+                Registrate
+              </Link>
+            </p>
+            <div>
+              <Link className={style.googleLogin} href='/google-login'>
+                <span className={style.text}>Iniciar sesión con Google</span>
+                <FcGoogle fontSize={30} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,18 @@
     Author: 🟣 Enoc Lima
 =====================================================*/
 
-
 'use client';
-import { useStyle } from "@/context/react/styleContext"
-import { useAppDispatch, useAppSelector } from "@/context/redux/typedHoocks";
-import { selectUser } from "@/context/redux/features/userSlice";
-import { useGetUserQuery } from "@/context/redux/api_handler/userApi";
-import { useGetDefaultsQuery } from "@/context/redux/api_handler/navigationApi";
+import { useStyle } from '@/context/react/styleContext';
+import { useAppDispatch, useAppSelector } from '@/context/redux/typedHoocks';
+import { selectUser } from '@/context/redux/features/userSlice';
+import { useGetUserQuery } from '@/context/redux/api_handler/userApi';
+import { useGetDefaultsQuery } from '@/context/redux/api_handler/navigationApi';
 
 export default function Home() {
-  const { screen } =  useStyle();
+  const { screen } = useStyle();
   const { isAuthenticated, user } = useAppSelector(selectUser);
   //const data = useGetUserQuery("character/84");
-  const data = useGetDefaultsQuery("character/84");
-  
-  return (
-    <div  >
-      {}
-    </div>
-  )
+  const data = useGetDefaultsQuery('/');
+
+  return <div>{}</div>;
 }
